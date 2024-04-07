077259507ac5df9a48b6aee4351ee1c8b181060e
ashmitchamoli <ashmitchamoli@outlook.com>
2024-04-07 22:23:24 +0530

    cleaned up code

--- PageRank/mapper.py
+++ PageRank/mapper.py
@@ -3,17 +3,11 @@
 
 import sys
 
-# lines = sys.stdin.readlines()
-# numNodes = len(lines)
-# lines = sys.stdin.read().split('\n')
-# numNodes = len(lines)
-
 for line in sys.stdin.readlines():
     line = line.strip()
     if line == '': continue
     page, myPageRank, *neighbors = line.split()
     print(line) # carry forward the graph structure
-    # print(f'{page}\tnumNodes\t{numNodes}') # pass on the number of nodes
     if neighbors[0] == '0': # there are no outgoing links
         continue
 
--- PageRank/reducer.py
+++ PageRank/reducer.py
@@ -6,13 +6,10 @@ import sys
 currPage = None
 currOutLinks = None
 incomingPageRankSum = 0
-# numNodes = None
-
 dampingFactor = 0.85
 
 def processInfo(info : str, 
                 incomingPageRankSum : float, 
-                # numNodes : int, 
                 currOutLinks : str):
     """
     Returns:
@@ -21,11 +18,10 @@ def processInfo(info : str,
     info = info.split('\t', maxsplit=1)
     if len(info) == 1: # info contains an incoming page rank
         incomingPageRankSum += float(info[0])
-    else: # info contains the current page info
-        # if info[0].strip() == 'numNodes': # info contains the number of nodes
-        #     numNodes = int(info[1])
-        # else: # info contains the outgoing links
+    else: # info contains the out going links
         currOutLinks = info[1].strip()
+    
+    # return updated values
     return incomingPageRankSum, currOutLinks
 
 for line in sys.stdin:
@@ -35,7 +31,7 @@ for line in sys.stdin:
     # * info will contain 1 element for incoming page rank and more than 1 for page info
     page, info = line.split('\t', maxsplit=1)
 
-    if page == currPage:
+    if page == currPage: # update using current info
         incomingPageRankSum, currOutLinks = processInfo(info, incomingPageRankSum, currOutLinks)
     else:
         if currPage is not None:
